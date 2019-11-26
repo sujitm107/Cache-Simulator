e1540b85f0bd9355c67253cd3522db45ab07ebc2
Sujit <sujit.mollet@rutgers.edu>
2019-11-26 04:42:59 -0500

    works for testcase 1

--- first/first.c
+++ first/first.c
@@ -35,7 +35,7 @@ int checkHit(struct cacheBlock** set, unsigned long tag, int associativity){
 	//printf("%lu\n", set[0]->tag);
 
 	for(int i = 0; i<associativity; i++){
-		printf("set: %lu, tagCompared: %lu ", set[i]->tag, tag);
+		//printf("set: %lu, tagCompared: %lu ", set[i]->tag, tag);
 			if(set[i]->valid != 1){
 				return 0;
 			}
@@ -56,13 +56,14 @@ void writeToCache(struct cacheBlock** set, unsigned long tag, int associativity)
 	temp->time = time+1;
 
 	int i = 0;
-	unsigned int minTime = 10000000;
+	unsigned int minTime = 1000000000;
 	int minIndex = 0;
 
 	for(i = 0; i<associativity; i++){
 		if(set[i]->valid != 1){
 			set[i] = temp;
 			set[i]->valid = 1;
+			//printf("Inserted in empty slot\n");
 			return;
 		}
 		if(minTime > set[i]->time){
@@ -79,17 +80,13 @@ void writeToCache(struct cacheBlock** set, unsigned long tag, int associativity)
 	if(i == associativity){ //returns an index
 		set[minIndex] = temp;
 		temp->valid = 1;
+		//printf("Inserted through replacement\n");
 	}
 
 }
 
 
 int main(int argc, char** argv){
-
-	int num_reads = 0;
-	int num_writes = 0;
-	int num_cache_hits = 0;
-	int num_cache_misses = 0;
 
 	int cache_size = atoi(argv[1]);
 	int block_size = atoi(argv[2]);
@@ -101,6 +98,14 @@ int main(int argc, char** argv){
 		associativity = cache_size/block_size;
 	}else{
 		associativity = argv[4][6] - '0';
+		for(int i = 0; i<associativity; i++){
+			if(i == log2(associativity)){
+				break;
+			}
+			if(i == associativity-1){
+				return 0;
+			}
+		}
 	}
 
 	int prefetch_size = atoi(argv[5]); 
@@ -124,18 +129,24 @@ int main(int argc, char** argv){
 
 	int offset_bits = log2(block_size);
 	int set_index_bits = log2(num_sets); //taking the log of the number of sets
-	int tag_bits = 48 - offset_bits - set_index_bits;
+	//int tag_bits = 48 - offset_bits - set_index_bits;
 
 	int mask = (1 << set_index_bits) - 1;
 
-	printf("Offset Bits: %d\n", offset_bits);
-	printf("Index Bits: %d\n", set_index_bits);
-	printf("Tag Bits: %d\n", tag_bits);
-	printf("Number of Sets: %d\n", num_sets);
-	printf("Prefetch Size: %d\n", prefetch_size);
-	printf("Associativity: %d\n", associativity);
+	// printf("Offset Bits: %d\n", offset_bits);
+	// printf("Index Bits: %d\n", set_index_bits);
+	// printf("Tag Bits: %d\n", tag_bits);
+	// printf("Number of Sets: %d\n", num_sets);
+	// printf("Prefetch Size: %d\n", prefetch_size);
+	// printf("Associativity: %d\n", associativity);
 
 //ALLOCATING CACHE
+	for(int k = 0; k<2; k++){
+		int num_reads = 0;
+		int num_writes = 0;
+		int num_cache_hits = 0;
+		int num_cache_misses = 0;
+
 		struct cacheBlock*** cache = malloc(num_sets*sizeof(struct cacheBlock**));
 
 		for(int r = 0; r<num_sets; r++){
@@ -160,37 +171,72 @@ int main(int argc, char** argv){
 			// 	num_writes += 1;
 				
 			// }
-			printf("%d\n", set_index);
-			if(checkHit(cache[set_index], tag, associativity) == 1){
-				printf("hit\n");
+			//printf("%d\n", set_index);
+			if(k == 0){ //WOUT PREFETCH
+				if(checkHit(cache[set_index], tag, associativity) == 1){
+					//printf("hit\n");
 
-				num_cache_hits++;
-				if(command == 'W'){
-					num_writes++;
-				}
-			} else {
-				printf("miss\n");
-
-
-
-				writeToCache(cache[set_index], tag, associativity);
-				num_cache_misses++;
-				if(command == 'W'){
-					num_reads++;
-					num_writes++;
+					num_cache_hits++;
+					if(command == 'W'){
+						num_writes++;
+					}
 				} else {
-					num_reads++;
+					//printf("miss\n");
+
+					writeToCache(cache[set_index], tag, associativity);
+					num_cache_misses++;
+					if(command == 'W'){
+						num_reads++;
+						num_writes++;
+					} else {
+						num_reads++;
+					}
+				}
+			 } else{ //PREFETCH
+				if(checkHit(cache[set_index], tag, associativity) == 1){
+					num_cache_hits++;
+					if(command == 'W'){
+						num_writes++;
+					}
+				} else {
+					//printf("%llu miss\n", address);
+					writeToCache(cache[set_index], tag, associativity);
+					num_cache_misses++;
+					if(command == 'W'){
+						num_reads++;
+						num_writes++;
+					} else {
+						num_reads++;
+					}
+
+					unsigned long long int prefetch_address = address;
+					for(int i = 0; i<prefetch_size; i++){
+						prefetch_address = prefetch_address + block_size;
+						//printf("%llx\n", prefetch_address);
+						set_index = (prefetch_address >> offset_bits) & mask;
+						tag = (prefetch_address >> offset_bits) >> set_index_bits;
+						if(checkHit(cache[set_index], tag, associativity) == 0){
+							writeToCache(cache[set_index], tag, associativity);
+							num_reads++;
+						}
+					}
 				}
 			}
-
 			
 		}
 
-		printf("no-prefetch\n");
+		if(k == 0){
+			printf("no-prefetch\n");
+		}
+		else{
+			printf("with-prefetch\n");
+		}
 		printf("Memory reads: %d\n", num_reads);
 		printf("Memory writes: %d\n", num_writes);
 		printf("Cache hits: %d\n", num_cache_hits);
 		printf("Cache misses: %d\n", num_cache_misses);
+
+	}
 	
 	
 	// printf("with-prefetch\n");
